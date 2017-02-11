339d71391411f64582d9779220ea5999b8cb8337
Sara Tracy <slttcb@mail.umsl.edu>
2017-02-10 23:44:51 -0600

    added styling

--- main.py
+++ main.py
@@ -19,23 +19,32 @@ class Handler(webapp2.RequestHandler):
     def render(self,template,**kw):
         self.write(self.render_str(template, **kw))
 
-class Art(db.model): #represent submission from user, inherits from db.model (creates entity)
-    pass 
+class Art(db.Model): #represent submission from user, inherits from db.model (creates entity)
+    title = db.StringProperty(required=True)#tells google this is string type
+    art = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)#date stamps submission-look in docs for more
+
     
 class MainPage(Handler):
-    def render_front(self, title="", art="", error="error"):
-        self.render("front.html", title=title, art=art, error=error) #pass in variables so 
+    def render_front(self, title="", art="", error=""):
+        arts = db.GqlQuery("SELECT * FROM Art "
+                            "ORDER BY created DESC ") #stores query
+
+        self.render("front.html", title=title, art=art, error=error, arts=arts) #pass in variables so 
                                                                 #they can be used in the form
 
     def get (self):
-        self.render("front.html")
+        self.render_front()
 
     def post(self):
         title=self.request.get("title")
         art=self.request.get("art")
 
         if title and art:
-            self.write("Thank you!")
+            a = Art(title=title, art=art)#get art object in success case
+            a.put() #stores new art object in database
+
+            self.redirect("/")#redirect back to empty form
         else:
             error="DOES NOT COMPUTE!!"
             self.render_front(title, art, error)#sends in error, title and art
